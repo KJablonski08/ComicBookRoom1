7bf8d719296d4e3dd60372b87e5a25746eae5e8e
kjablonski08 <kjablonski08@gmail.com>
2019-08-15 22:56:00 -0400

    linked up CSS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < Sinatra::Base
 
+    configure do 
+        set :views, 'app/views'
+        set :public_folder, 'public'
+    end 
+
     get '/' do 
         "Hello World"
+        erb :index
     end  
 
 end 
